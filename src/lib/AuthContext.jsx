08b5b091dfdf021a4d92efaee18f764ffd7fd4d1
omenpotter <145251134+omenpotter@users.noreply.github.com>
2026-03-08 18:18:33 +0300

    Update AuthContext.jsx

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -8,8 +8,8 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoadingAuth, setIsLoadingAuth] = useState(true);
-  const [isLoadingPublicSettings, setIsLoadingPublicSettings] = useState(true);
+  const [isLoadingAuth, setIsLoadingAuth] = useState(false);
+  const [isLoadingPublicSettings, setIsLoadingPublicSettings] = useState(false);
   const [authError, setAuthError] = useState(null);
   const [appPublicSettings, setAppPublicSettings] = useState(null); // Contains only { id, public_settings }
 
